147e11de322893911fef9584ede1bf246ea3f73a
Skylar-Brockbank <91647723+Skylar-Brockbank@users.noreply.github.com>
2024-10-02 16:45:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -541,8 +541,8 @@ let screen = document.getElementById('screen');
 let brush = screen.getContext("2d");
 screen.style.boxSizing = 'border-box';
 
-const x = 500;
-const y= 250;
+const x = 200;
+const y= 100;
 const noiseConstants = [80,65];
 
 let q;
